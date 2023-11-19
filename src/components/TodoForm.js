d3ee2e990b545b9954abf663fa41a43458caf611
namanica <veronichkapilipna@gmail.com>
2023-11-19 21:13:05 +0200

    created a function for todo-form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,22 +1,23 @@
 import React, {useState} from 'react'
 
-export const TodoForm = ({addTodo}) => {
-    const [value, setValue] = useState('');
-
-    const handleSubmit = (e) => {
-      // prevent default action
-        e.preventDefault();
-        if (value) {
-          // add todo
-          addTodo(value);
-          // clear form after submission
-          setValue('');
-        }
-      };
+export const TodoForm = () => {
+  const [value, setValue] = useState("")
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(value);
+  }
   return (
-    <form onSubmit={handleSubmit} className="TodoForm">
-    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='What is the task today?' />
-    <button type="submit" className='todo-btn'>Add Task</button>
-  </form>
+    <form
+    className='TodoForm'
+    onSubmit={handleSubmit}>
+      <input
+      type='text'
+      className='todo-input'
+      placeholder='What are we gonna do today?'
+      onChange={(e) => setValue(e.target.value)}/>
+      <button
+      type='submit'
+      className='todo-button'>Add task</button>
+    </form>
   )
 }
